8e6932a69d02d7e313eec823e41124400313b14a
LuLu-89 <lucy.ly89@gmail.com>
2016-05-09 22:56:14 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -38,6 +38,12 @@ cacheSolve <- function(x, ...) {
 
 ## Testing the function:
 m <- matrix(c(2, 4, 3, 1), nrow = 2, ncol = 2)
+##      [,1] [,2]
+## [1,]   2    3
+## [2,]   4    1
 
 ## Inversing the matrix:
 solve(m)
+##      [,1] [,2]
+## [1,] -0.1  0.3
+## [2,]  0.4 -0.2
